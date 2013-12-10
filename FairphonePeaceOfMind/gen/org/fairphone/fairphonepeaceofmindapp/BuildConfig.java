2e9db8cbeb0b5ddeae46a2e2c8d459c89929bc4b
Kim Hansen <kim@kwamecorp.com>
2013-12-10 12:33:07 +0000

    Cleanup - removing bin and gen folders from repository

--- FairphonePeaceOfMind/gen/org/fairphone/fairphonepeaceofmindapp/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.fairphone.fairphonepeaceofmindapp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
