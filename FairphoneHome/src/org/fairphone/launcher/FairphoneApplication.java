1477b89e54d81bad30fd041e35844dee092b3edf
Jose Pascoal <jose.pascoal@kwamecorp.com>
2014-04-10 11:27:01 +0100

    - Remove ACRA

--- FairphoneHome/src/org/fairphone/launcher/FairphoneApplication.java
+++ FairphoneHome/src/org/fairphone/launcher/FairphoneApplication.java
@@ -15,28 +15,11 @@
  */
 package org.fairphone.launcher;
 
-import org.acra.ACRA;
-import org.acra.ReportField;
-import org.acra.annotation.ReportsCrashes;
-
-@ReportsCrashes(formKey = "dDFyQmdEbmtvU0UwZUNRVDY1WEc3WWc6MQ", logcatFilterByPid = true, logcatArguments = {
-        "-t", "500"
-}, customReportContent = {
-        ReportField.REPORT_ID, ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.PACKAGE_NAME, ReportField.FILE_PATH,
-        ReportField.PHONE_MODEL, ReportField.BRAND, ReportField.PRODUCT, ReportField.ANDROID_VERSION, ReportField.BUILD, ReportField.TOTAL_MEM_SIZE,
-        ReportField.AVAILABLE_MEM_SIZE, ReportField.CUSTOM_DATA, ReportField.IS_SILENT, ReportField.STACK_TRACE, ReportField.INITIAL_CONFIGURATION,
-        ReportField.CRASH_CONFIGURATION, ReportField.DISPLAY, ReportField.USER_COMMENT, ReportField.USER_EMAIL, ReportField.USER_APP_START_DATE,
-        ReportField.USER_CRASH_DATE, ReportField.DUMPSYS_MEMINFO, ReportField.LOGCAT, ReportField.INSTALLATION_ID, ReportField.DEVICE_FEATURES,
-        ReportField.ENVIRONMENT, ReportField.SHARED_PREFERENCES, ReportField.SETTINGS_SYSTEM, ReportField.SETTINGS_SECURE, ReportField.EVENTSLOG,
-        ReportField.RADIOLOG
-})
 public class FairphoneApplication extends LauncherApplication
 {
     @Override
     public void onCreate()
     {
-        ACRA.init(this);
-
         super.onCreate();
     }
 }
